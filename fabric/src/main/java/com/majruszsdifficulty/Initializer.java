c19875d2a472cc2fb43fd3112ee28f7483140283
Santy <42287552+majorsanty@users.noreply.github.com>
2024-05-03 23:57:09 -0300

    Fixes

--- fabric/src/main/java/com/majruszsdifficulty/Initializer.java
+++ fabric/src/main/java/com/majruszsdifficulty/Initializer.java
@@ -22,15 +22,17 @@ public class Initializer implements ModInitializer {
 	public void onInitialize() {
 		MajruszsDifficulty.HELPER.register();
 
+		/*
 		BiomeModifications.addFeature( IS_END, GenerationStep.Decoration.UNDERGROUND_ORES, MajruszsDifficulty.ENDERIUM_ORE_PLACED_FEATURE );
 		BiomeModifications.addFeature( IS_END, GenerationStep.Decoration.UNDERGROUND_ORES, MajruszsDifficulty.ENDERIUM_ORE_LARGE_PLACED_FEATURE );
 		BiomeModifications.addFeature( IS_END, GenerationStep.Decoration.UNDERGROUND_ORES, MajruszsDifficulty.FRAGILE_END_STONE_PLACED_FEATURE );
 		BiomeModifications.addFeature( IS_END, GenerationStep.Decoration.UNDERGROUND_ORES, MajruszsDifficulty.FRAGILE_END_STONE_LARGE_PLACED_FEATURE );
 		BiomeModifications.addFeature( IS_END, GenerationStep.Decoration.UNDERGROUND_ORES, MajruszsDifficulty.INFESTED_END_STONE_PLACED_FEATURE );
+		 */
 
 		BiomeModifications.addSpawn( IS_OVERWORLD, MobCategory.MONSTER, MajruszsDifficulty.CURSED_ARMOR_ENTITY.get(), 30, 1, 3 );
 		BiomeModifications.addSpawn( IS_OVERWORLD, MobCategory.MONSTER, MajruszsDifficulty.ILLUSIONER_ENTITY.get(), 8, 1, 1 );
 		BiomeModifications.addSpawn( IS_OVERWORLD, MobCategory.MONSTER, MajruszsDifficulty.TANK_ENTITY.get(), 3, 1, 1 );
-		BiomeModifications.addSpawn( IS_NETHER, MobCategory.MONSTER, MajruszsDifficulty.CERBERUS_ENTITY.get(), 1, 1, 1 );
+		BiomeModifications.addSpawn( IS_OVERWORLD, MobCategory.MONSTER, MajruszsDifficulty.CERBERUS_ENTITY.get(), 1, 1, 1 );
 	}
 }
